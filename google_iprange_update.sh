eb441f866885b0486e202483261d656aad10fd68
verybadsoldier <vbs@springrts.de>
2019-02-22 08:55:26 +0100

    delete old rules first (in case stuff like TCP_PORT changed)

--- google_iprange_update.sh
+++ google_iprange_update.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
 
 IPSET_NAME_TMP="${IPSET_NAME}_TMP"
+IPTABLES_COMMENT="Check for Google IP"
 
 ipset -! -q create "$IPSET_NAME" nethash
 
 iptables -n --list "$CHAIN_NAME" >/dev/null 2>&1
-if [ $? -eq 1 ]; then
+if [ $? -eq 0 ]; then
+    iptables -F "$CHAIN_NAME"
+else
     iptables -N "$CHAIN_NAME" 2> /dev/null
-    iptables -A "$CHAIN_NAME" -m set ! --match-set "$IPSET_NAME" src -j DROP
-
-    for i in $TARGET_CHAINS; do
-        iptables -I "$i" -p tcp -m tcp --dport $TCP_PORT -j "$CHAIN_NAME"
-    done
 fi
+
+iptables -A "$CHAIN_NAME" -m set ! --match-set "$IPSET_NAME" src -j DROP
+
+cur_rules=$(iptables-save | grep -e "--comment \"$IPTABLES_COMMENT\"")
+IFS=$'\n'
+for i in $cur_rules; do
+    i="${i:3}"
+    i=($i)
+    iptables -D ${i[@]}
+done
+unset IFS
+
+for i in $TARGET_CHAINS; do
+    iptables -I "$i" -p tcp -m tcp --dport $TCP_PORT -j "$CHAIN_NAME" -m comment --comment "$IPTABLES_COMMENT"
+done
 
 ipset -! -q destroy "$IPSET_NAME_TMP"
 ipset create "$IPSET_NAME_TMP" nethash
